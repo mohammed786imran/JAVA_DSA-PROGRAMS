c407e2729f1394bafa3a233a88acafe08aaa4d1b
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-01-05 18:42:23 +0530

    Create Search Insert Position.java

---
+++ JAVA_DSA-PROGRAMS/Search Insert Position.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0; // Left boundary of the search range
+        int right = nums.length - 1; // Right boundary of the search range
+        
+        while (left <= right) { // Continue searching until the range is valid
+            int mid = left + (right - left) / 2; // Avoid overflow by calculating the midpoint this way
+            if (nums[mid] == target) {
+                return mid; // Return index if target is found
+            } else if (nums[mid] < target) {
+                left = mid + 1; // Narrow the search to the right half
+            } else {
+                right = mid - 1; // Narrow the search to the left half
+            }
+        }
+        return left; // Return the insertion position when the loop ends
+    }
+}
