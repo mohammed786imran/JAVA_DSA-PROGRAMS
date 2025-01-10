d76b16070401f071ca3aac55983006ff56f19004
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-01-10 18:47:58 +0530

    Create Num_Pat4.java

---
+++ NUMBER-PATTERNS/Num_Pat4.java
@@ -0,0 +1,18 @@
+package NUMBERPATTERN;
+
+public class NUM4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count=0;
+		for(int i=1;i<=4;i++)
+		{
+		for(int j=1;j<=i;j++)
+		{
+		System.out.print(count);
+		count++;
+		}
+		System.out.println();
+		}
+	}
+}
