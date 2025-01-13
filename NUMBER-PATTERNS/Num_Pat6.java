1056e191bd91f626d89da26df12437a35a6ffd05
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-01-13 18:18:15 +0530

    Create Num_Pat6.java

---
+++ NUMBER-PATTERNS/Num_Pat6.java
@@ -0,0 +1,19 @@
+package NUMBERPATTERN;
+
+public class NUM6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=4;i++)
+		{
+		int no=i;
+		for(int j=1;j<=i;j++)
+		{
+		System.out.print(no+" ");
+		no=no+4-j;
+		}
+		System.out.println();
+		}
+	}
+
+}
