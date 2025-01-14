0a5eff848d2a875c293427afe71125b67ee9d566
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-01-14 19:32:47 +0530

    Create Num_Pat7.java

---
+++ NUMBER-PATTERNS/Num_Pat7.java
@@ -0,0 +1,18 @@
+package NUMBERPATTERN;
+
+public class NUM7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=4;i++)
+		{
+		int no=i;
+		for(int j=1;j<=i;j++)
+		{
+		System.out.print(no+" ");
+		no=no+4;
+		}
+		System.out.println();
+		}
+	}
+}
