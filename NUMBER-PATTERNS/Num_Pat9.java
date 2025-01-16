5143cdd363993ebfc90b36b18345bd2e6df6ac99
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-01-16 18:20:26 +0530

    Create Num_Pat9.java

---
+++ NUMBER-PATTERNS/Num_Pat9.java
@@ -0,0 +1,16 @@
+package NUMBERPATTERN;
+
+public class NUM9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=4;i>=0;i--)
+		{
+		for(int j=4;j>=i;j--)
+		{
+		System.out.print(j);
+		}
+		System.out.println();
+		}
+	}
+}
