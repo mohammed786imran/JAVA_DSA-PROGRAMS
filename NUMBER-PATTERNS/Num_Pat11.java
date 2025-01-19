ed4346468a56aa6f2ce8c15471b012ec892ebfde
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-01-19 18:24:48 +0530

    Create Num_Pat11.java

---
+++ NUMBER-PATTERNS/Num_Pat11.java
@@ -0,0 +1,18 @@
+package NUMBERPATTERN;
+
+public class num11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<=4;i++)
+		{
+		int alphabet = 65;
+		for(int j=0;j<=i;j++)
+		{
+		System.out.print(i+j);
+		}
+		System.out.println();
+		}
+	}
+
+}
