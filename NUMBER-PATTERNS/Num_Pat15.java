29ba92e0557dc68fc13f5e2778f89afc8c5b8a38
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-01-24 19:15:42 +0530

    Create Num_Pat15.java

---
+++ NUMBER-PATTERNS/Num_Pat15.java
@@ -0,0 +1,24 @@
+package NUMBERPATTERN;
+
+public class NUM15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int row=5, i, j, space, num;
+	      for(i=0; i<row; i++)
+	      {
+	         for(space=row; space>i; space--)
+	         {
+	            System.out.print(" ");
+	         }
+	         num=1;
+	         for(j=0; j<=i; j++)
+	         {
+	            System.out.print(num+ " ");
+	            num = num*(i-j)/(j+1);
+	         }
+	         System.out.println();
+	      }
+	}
+
+}
