ae424f055706474b7e93da222bdce788114617b8
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-02-09 07:49:23 +0530

    Create ARITHEMATICOPERATOR.java

---
+++ MYPACKAGE/MYPROGRAMS/ARITHEMATICOPERATOR.java
@@ -0,0 +1,19 @@
+package MYPACKAGE;
+
+public class ARITHEMATICOPERATOR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int num1=10,num2=5;
+      int sum=num1+num2;
+      int sub=num1-num2;
+      int mul=num1*num2;
+      int div=num1/num2;
+      int mod=num1%num2;
+  System.out.println("sum:"+sum);
+  System.out.println("sub:"+sub);
+  System.out.println("mul:"+mul);
+  System.out.println("div:"+div);
+  System.out.println("mod:"+mod);
+	}
+}
