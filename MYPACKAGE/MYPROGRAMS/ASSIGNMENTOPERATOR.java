be7c63fe81bb50c3ccb4666f8358370229eb2500
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-02-10 07:16:15 +0530

    Create ASSIGNMENTOPERATOR.java

---
+++ MYPACKAGE/MYPROGRAMS/ASSIGNMENTOPERATOR.java
@@ -0,0 +1,11 @@
+package MYPACKAGE;
+
+public class ASSIGNMENTOPERATOR {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int num1=5,num2;
+     System.out.println(num1);
+     num2=num1;
+     System.out.print(num2);
+	}
+}
