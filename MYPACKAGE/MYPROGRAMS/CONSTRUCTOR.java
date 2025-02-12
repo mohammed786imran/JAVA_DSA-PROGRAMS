5529a03c4f172b0102fe8f5333ece5d06e3f9e38
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-02-12 07:12:48 +0530

    Create CONSTRUCTOR.java

---
+++ MYPACKAGE/MYPROGRAMS/CONSTRUCTOR.java
@@ -0,0 +1,14 @@
+package MYPACKAGE;
+
+public class CONSTRUCTOR {
+
+	public static void main(String[] args) {
+		CONSTRUCTOR MY=new CONSTRUCTOR();
+	}
+		// TODO Auto-generated method stub
+CONSTRUCTOR()
+{
+	System.out.print("CONSTRUCTOR IS CREATED");
+	}
+
+}
