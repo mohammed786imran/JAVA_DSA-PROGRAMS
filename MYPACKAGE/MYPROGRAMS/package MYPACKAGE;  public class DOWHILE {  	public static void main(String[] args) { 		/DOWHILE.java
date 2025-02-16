be852398e235c9eb019f2b17daa9c0092879044a
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-02-16 08:19:01 +0530

    Create  DOWHILE.java

---
+++ MYPACKAGE/MYPROGRAMS/package MYPACKAGE;  public class DOWHILE {  	public static void main(String[] args) { 		/DOWHILE.java
@@ -0,0 +1,15 @@
+package MYPACKAGE;
+
+public class DOWHILE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		do
+		{
+			System.out.println(i);
+			i++;
+		}while(i<=4);
+	}
+
+}
