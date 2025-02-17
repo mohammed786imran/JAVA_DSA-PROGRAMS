31217367bfbc0044044ace3e0a3a0e19bb8e6c65
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-02-17 08:48:21 +0530

    Create EVENODD.java

---
+++ MYPACKAGE/MYPROGRAMS/EVENODD.java
@@ -0,0 +1,18 @@
+package MYPACKAGE;
+
+public class EVENODD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num=13;
+if(num%2==0)
+{
+	System.out.print("even number:"+num);
+}
+else
+	{
+	System.out.print("odd number:"+num);
+	}
+	}
+
+}
