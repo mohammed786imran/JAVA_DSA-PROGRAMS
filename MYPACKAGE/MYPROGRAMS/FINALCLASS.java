e599a73d7f37867580acee291d2874e833d016df
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-02-19 07:16:37 +0530

    Create FINALCLASS.java

---
+++ MYPACKAGE/MYPROGRAMS/FINALCLASS.java
@@ -0,0 +1,23 @@
+package MYPACKAGE;
+
+final public class FINALCLASS {
+     FINALCLASS()
+     {
+    	 System.out.print("final CLASS");
+     }
+}
+class child extends FINALCLASS
+{
+	child()
+	{
+		System.out.println("child");
+	}
+}
+class finaltest2
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	}
+
+}
