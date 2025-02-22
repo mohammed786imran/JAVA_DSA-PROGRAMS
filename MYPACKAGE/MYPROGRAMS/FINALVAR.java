df8aef7f328b3cb2e2bb211ba9c76dad6f08a4c1
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-02-22 09:40:56 +0530

    Create FINALVAR.java

---
+++ MYPACKAGE/MYPROGRAMS/FINALVAR.java
@@ -0,0 +1,17 @@
+package MYPACKAGE;
+
+public class FINALVAR {
+    final int i=10;
+    FINALVAR()
+    {
+   int i=10+10;
+    	System.out.print(i);
+    }
+}
+class finaltest
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FINALVAR var=new FINALVAR();
+	}
+}
