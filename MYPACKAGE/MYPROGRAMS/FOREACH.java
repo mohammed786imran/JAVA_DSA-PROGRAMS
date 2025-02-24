8d20227ee8b914dd335a38a827748fb265a2fd52
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-02-24 07:51:12 +0530

    Create FOREACH.java

---
+++ MYPACKAGE/MYPROGRAMS/FOREACH.java
@@ -0,0 +1,18 @@
+package MYPACKAGE;
+
+public class FOREACH {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int array[]=new int [10];
+		{
+	for(int i : array)
+		System.out.println(i);
+	}
+	int arrays[]= {1,2,3,4,5,6,7,8,9,10};
+	{
+for(int element : arrays)
+System.out.print(" "+element);
+}
+	}
+}
