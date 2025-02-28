a14bb1d1462c0b790ccddbdd7addec9f9ca9d719
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-02-28 16:42:02 +0530

    Create IMPLICIT.java

---
+++ MYPACKAGE/MYPROGRAMS/IMPLICIT.java
@@ -0,0 +1,13 @@
+package MYPACKAGE;
+
+public class IMPLICIT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float x;
+int a=21,b=22;
+x=a+b;
+System.out.print("x value is:"+x);
+	}
+
+}
