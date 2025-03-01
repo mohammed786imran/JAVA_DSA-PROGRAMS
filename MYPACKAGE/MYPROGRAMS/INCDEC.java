4e819897043371cbd326cb5243da55b41322c576
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-03-01 12:30:53 +0530

    Create INCDEC.java

---
+++ MYPACKAGE/MYPROGRAMS/INCDEC.java
@@ -0,0 +1,18 @@
+package MYPACKAGE;
+
+public class INCDEC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=5,b;
+b=a++;
+System.out.println("value of b is:"+b);
+b=--a;
+System.out.println("value of b is:"+b);
+b=a--;
+System.out.println("value of b is:"+b);
+a=a--;
+System.out.println("value of b is:"+b);
+	}
+
+}
