fe4945edad76107c444f2c54ebfa49e5419fe48c
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-03-03 14:25:48 +0530

    Create HollowPyramid.java

---
+++ MYPACKAGE/MYPROGRAMS/HollowPyramid.java
@@ -0,0 +1,26 @@
+package MYPACKAGE;
+import java.util.Scanner;
+		public class HollowPyramid {
+		    public static void main(String[] args) {
+		        Scanner scanner = new Scanner(System.in);
+		        System.out.print("Enter the height of the pyramid: ");
+		        int n = scanner.nextInt();
+		        scanner.close();
+
+		        for (int i = 1; i <= n; i++) {
+		            for (int j = 1; j <= n - i; j++) {
+		                System.out.print(" "); // Print spaces for alignment
+		            }
+		            for (int j = 1; j <= (2 * i - 1); j++) {
+		                // Print '*' at boundaries, otherwise space
+		                if (j == 1 || j == (2 * i - 1) || i == n) {
+		                    System.out.print("*");
+		                } else {
+		                    System.out.print(" ");
+		                }
+		            }
+		            System.out.println(); // Move to next line
+		        }
+	}
+
+}
