6d53b6cc4cab2377d253760081a345a976ea16a0
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-03-04 08:28:09 +0530

    Update HollowPyramid.java

--- MYPACKAGE/MYPROGRAMS/HollowPyramid.java
+++ MYPACKAGE/MYPROGRAMS/HollowPyramid.java
@@ -6,8 +6,7 @@ import java.util.Scanner;
 		        System.out.print("Enter the height of the pyramid: ");
 		        int n = scanner.nextInt();
 		        scanner.close();
-
-		        for (int i = 1; i <= n; i++) {
+                      for (int i = 1; i <= n; i++) {
 		            for (int j = 1; j <= n - i; j++) {
 		                System.out.print(" "); // Print spaces for alignment
 		            }
@@ -21,6 +20,5 @@ import java.util.Scanner;
 		            }
 		            System.out.println(); // Move to next line
 		        }
-	}
-
-}
+	             }
+                  }
