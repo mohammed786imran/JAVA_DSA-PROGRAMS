4d46778bd93b3e7c609dcc30d78344cba0070461
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-03-05 14:12:07 +0530

    Create INTERFACEVAR.java

---
+++ MYPACKAGE/MYPROGRAMS/INTERFACEVAR.java
@@ -0,0 +1,18 @@
+package MYPACKAGE;
+
+public interface INTERFACEVAR {
+   int x=10;
+interface B
+{
+	int x=100;
+}
+class test implements INTERFACEVAR,B
+{
+}
+	public static void main(String args[])
+	{
+		System.out.println(x);
+		System.out.println(INTERFACEVAR.x);
+		System.out.print(B.x);
+	}
+}
