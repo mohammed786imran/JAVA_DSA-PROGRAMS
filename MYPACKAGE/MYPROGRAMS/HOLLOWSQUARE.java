262a71a40afb0ecc9b7f11010e798965847bc570
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-03-05 14:13:25 +0530

    Update HOLLOWSQUARE.java

--- MYPACKAGE/MYPROGRAMS/HOLLOWSQUARE.java
+++ MYPACKAGE/MYPROGRAMS/HOLLOWSQUARE.java
@@ -1,9 +1,6 @@
 package MYPACKAGE;
-
 public class HOLLOWSQUARE {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		for(int i=1;i<=4;i++)
 		{
 		for(int j=1;j<=4;j++)
