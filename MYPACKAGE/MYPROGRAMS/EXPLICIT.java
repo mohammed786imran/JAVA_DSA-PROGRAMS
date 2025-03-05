e57cbef992166a39d7f15dbcb7681734221cf1a0
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-03-05 14:20:13 +0530

    Update EXPLICIT.java

--- MYPACKAGE/MYPROGRAMS/EXPLICIT.java
+++ MYPACKAGE/MYPROGRAMS/EXPLICIT.java
@@ -1,13 +1,9 @@
 package MYPACKAGE;
-
 public class EXPLICIT {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args){
 		int x;
 		double a=21,b=22;
 		x=(int)(a+b);
 		System.out.print("x value is:"+x);
 	}
-
 }
