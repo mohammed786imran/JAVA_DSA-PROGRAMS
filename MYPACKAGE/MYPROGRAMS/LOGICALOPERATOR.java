5480d5be5babc82c9f8470e88a792801e16328df
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-03-11 15:07:05 +0530

    Create LOGICALOPERATOR.java

---
+++ MYPACKAGE/MYPROGRAMS/LOGICALOPERATOR.java
@@ -0,0 +1,15 @@
+package MYPACKAGE;
+
+public class LOGICALOPERATOR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int a=3,b=6,c=9;
+       Boolean result;
+       result=(a>b)||(c>a);
+       System.out.println(result);
+       result=(a>b)&&(c>a);
+       System.out.println(result);
+	}
+
+}
