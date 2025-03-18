050b5d9b2c523509118ef82a8054e623aab02eb4
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-03-18 08:23:25 +0530

    Create NEWCLASS.java

---
+++ MYPACKAGE/MYPROGRAMS/NEWCLASS.java
@@ -0,0 +1,15 @@
+package MYPACKAGE;
+
+public class NEWCLASS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	NEWCLASS MYSNEWCLASS=new NEWCLASS("TOMMY");
+	}// TODO Auto-generated method stub
+public NEWCLASS(String name)
+{
+	System.out.print("passed name is:" +name);
+}
+
+	}
+
