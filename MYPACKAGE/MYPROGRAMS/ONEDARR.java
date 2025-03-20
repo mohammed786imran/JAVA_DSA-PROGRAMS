f3660a30460e1f39f7393c11c9d0b5f31f00634d
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-03-20 15:26:04 +0530

    Create ONEDARR.java

---
+++ MYPACKAGE/MYPROGRAMS/ONEDARR.java
@@ -0,0 +1,23 @@
+package MYPACKAGE;
+
+public class ONEDARR {
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int month_days[];
+			month_days=new int[12];
+			month_days[0]=31;
+			month_days[1]=31;
+			month_days[2]=31;
+			month_days[3]=31;
+			month_days[4]=31;
+			month_days[5]=31;
+			month_days[6]=31;
+			month_days[7]=31;
+			month_days[8]=31;
+			month_days[9]=31;
+			month_days[10]=31;
+			month_days[11]=31;
+			System.out.println("april has"+month_days[3]+"day");
+		}
+			
+}
