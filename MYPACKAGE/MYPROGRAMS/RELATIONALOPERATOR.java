2039cbab369acfe8b042c6b8ec3691382f963987
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-03-22 08:12:44 +0530

    Create RELATIONALOPERATOR.java

---
+++ MYPACKAGE/MYPROGRAMS/RELATIONALOPERATOR.java
@@ -0,0 +1,19 @@
+package MYPACKAGE;
+
+public class RELATIONALOPERATOR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int a=10,b=20;
+     boolean res;
+     res=a==b;
+     System.out.println("a==b :"+res);
+     res=a!=b;
+     System.out.println("a!=b :"+res);
+     res=a>=b;
+     System.out.println("a>=b :"+res);
+     res=a<=b;
+     System.out.println("a<=b :"+res);
+	}
+
+}
