4538af65e0466c9a2da74b442e709d4a022d651b
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-03-25 14:36:54 +0530

    Create RUNNABLETHREAD.java

---
+++ MYPACKAGE/MYPROGRAMS/RUNNABLETHREAD.java
@@ -0,0 +1,17 @@
+package MYPACKAGE;
+
+public class RUNNABLETHREAD implements Runnable {
+     public void run()
+     {
+    	 System.out.println("CHILD THREAD");
+     }
+     public class thread
+     {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     RUNNABLETHREAD t=new RUNNABLETHREAD();
+         Thread obj=new Thread();
+                obj.start();
+	}
+}
+}
