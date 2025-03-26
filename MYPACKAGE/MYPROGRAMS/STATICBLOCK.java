f0c1eaa2f49442bb9700325feae0eefd5c382309
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-03-26 13:47:03 +0530

    Create STATICBLOCK.java

---
+++ MYPACKAGE/MYPROGRAMS/STATICBLOCK.java
@@ -0,0 +1,13 @@
+ package MYPACKAGE;
+
+public class STATICBLOCK {
+   static
+   {
+	   System.out.print("THIS IS STATIC BLOCK");
+	  }
+   public static void main(String args[])
+   {
+	   
+   }
+}
+
