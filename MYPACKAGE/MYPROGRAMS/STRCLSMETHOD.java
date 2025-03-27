cffa7cac1a39f5adefa20dbeb41543411c38db17
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-03-28 02:18:15 +0530

    Create STRCLSMETHOD.java

---
+++ MYPACKAGE/MYPROGRAMS/STRCLSMETHOD.java
@@ -0,0 +1,15 @@
+package MYPACKAGE;
+
+public class STRCLSMETHOD {
+	public static void main(String[] args) {
+     String s="CME CSE";
+     System.out.println(s.toUpperCase());
+     System.out.println(s.toLowerCase());
+     System.out.println(s.trim());
+     System.out.println(s.charAt(0));
+     System.out.println(s.length());
+     System.out.println(s.replace("ME","SG"));
+		// TODO Auto-generated method stub
+		}
+
+}
