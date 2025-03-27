7e97ddaf9f58b2ceb7abaf48b3de692b6fea2de3
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-03-27 07:07:20 +0530

    Create STATICMETHOD.java

---
+++ JAVA_DSA-PROGRAMS/STATICMETHOD.java
@@ -0,0 +1,22 @@
+package MYPACKAGE;
+
+public class STATICMETHOD {
+    int i;
+    static String name="main";
+    static void update()
+    {
+    	name="cme class";
+    System.out.print("iam in ");
+    }
+    void display()
+    {
+    	System.out.print(name);
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    update();
+    STATICMETHOD obj=new STATICMETHOD();
+    obj.display();
+	}
+
+}
