35468ce8b6714672f0be39883a880a19d9bcc6ff
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-03-29 08:21:04 +0530

    Create STUDENTS.java

---
+++ MYPACKAGE/MYPROGRAMS/STUDENTS.java
@@ -0,0 +1,24 @@
+package MYPACKAGE;
+
+public class STUDENTS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		STUDENTS S1=new STUDENTS(111,"MOHAMMED");
+		STUDENTS S2=new STUDENTS(222,"IMRAN");
+		S1.display();
+		S2.display();
+	}
+int id;
+String name;
+STUDENTS(int i,String n)
+{
+	id=i;
+	name=n;
+}
+void display()
+{
+	System.out.println(id+" "+name);
+}
+	}
+
