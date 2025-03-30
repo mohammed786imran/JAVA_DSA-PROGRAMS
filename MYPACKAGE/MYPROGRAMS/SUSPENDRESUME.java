289ddda65dbd36766d9d2b4961b830513dfb9d57
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-03-30 07:28:56 +0530

    Create SUSPENDRESUME.java

---
+++ MYPACKAGE/MYPROGRAMS/SUSPENDRESUME.java
@@ -0,0 +1,32 @@
+package MYPACKAGE;
+
+public class SUSPENDRESUME extends Thread {
+     public void run()
+     {
+    	 for(int i=1;i<5;i++)
+    	 {
+    		 try
+    		 {
+    			 Thread.sleep(5000);
+    			 System.out.println(Thread.currentThread().getName());
+    		 }
+    		 catch(Exception e)
+    		 {
+    			 System.out.println(e);
+    		 }
+    		 System.out.println(i);
+    	 }
+     }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      SUSPENDRESUME t1=new SUSPENDRESUME();
+      SUSPENDRESUME t2=new SUSPENDRESUME();
+      SUSPENDRESUME t3=new SUSPENDRESUME();
+                    t1.start();
+                    t2.start();
+                    t2.suspend();
+                    t3.start();
+                    t2.resume();
+	}
+
+}
