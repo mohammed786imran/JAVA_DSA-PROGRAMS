587f634ab25c5d0e381b8038b31a990676d39847
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-04-04 06:47:33 +0530

    Create SWAPWAS.java

---
+++ MYPACKAGE/MYPROGRAMS/SWAPWAS.java
@@ -0,0 +1,15 @@
+package MYPACKAGE;
+
+public class SWAPWAS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3,b=5;
+		a=a^b;
+		b=a^b;
+		a=a^b;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
