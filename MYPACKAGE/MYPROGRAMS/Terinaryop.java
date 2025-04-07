3819f5d9e324fb6eb86e89ee7313638020055f75
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-04-07 07:43:43 +0530

    Create Terinaryop.java

---
+++ MYPACKAGE/MYPROGRAMS/Terinaryop.java
@@ -0,0 +1,12 @@
+package MYPACKAGE;
+
+public class Terinaryop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10,b;
+b=(a==1)?20:30;
+System.out.println("value of b is:" +b);
+	}
+
+}
