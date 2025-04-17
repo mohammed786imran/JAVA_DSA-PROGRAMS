344ad6ecd0f0bc2fe1dd69419cd33ef5e0a3cb72
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-04-17 09:56:19 +0530

    Create TWODARR.java

---
+++ MYPACKAGE/MYPROGRAMS/TWODARR.java
@@ -0,0 +1,27 @@
+package MYPACKAGE;
+
+public class TWODARR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int twod[][]=new int[4][5];
+int i,j,k=0;
+for(i=0;i<4;i++)
+{
+	for(j=0;j<5;j++)
+	{
+		twod[i][j]=k;
+		k++;
+	}
+}
+for(i=0;i<4;i++)
+{
+	for(j=0;j<5;j++)
+	{
+		System.out.print(twod[i][j]+" ");
+	}
+	System.out.println();
+	}
+
+}
+	}
