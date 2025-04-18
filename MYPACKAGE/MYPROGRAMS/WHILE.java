406ab0a2c5bc1ce64621e593f3147e11d7312746
MOHAMMED IMRAN <155433147+mohammed786imran@users.noreply.github.com>
2025-04-18 13:48:49 +0530

    Create WHILE.java

---
+++ MYPACKAGE/MYPROGRAMS/WHILE.java
@@ -0,0 +1,15 @@
+package MYPACKAGE;
+
+public class WHILE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=1;
+while(i<=4)
+{
+	System.out.println(i);
+	i++;
+}
+	}
+
+}
